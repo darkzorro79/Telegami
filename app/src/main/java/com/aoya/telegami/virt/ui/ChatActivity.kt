f23b81dd2a460dbd07a525e78bf7c60f28935cf7
Aoya <gitya@sl.yohli.me>
2026-03-20 00:09:06 +0800

    chore: virt/ui/ChatActivity cleanup

--- app/src/main/java/com/aoya/telegami/virt/ui/ChatActivity.kt
+++ app/src/main/java/com/aoya/telegami/virt/ui/ChatActivity.kt
@@ -1,6 +1,5 @@
 package com.aoya.telegami.virt.ui
 
-import de.robv.android.xposed.XposedHelpers.callMethod
 import de.robv.android.xposed.XposedHelpers.getBooleanField
 import de.robv.android.xposed.XposedHelpers.getLongField
 import de.robv.android.xposed.XposedHelpers.setBooleanField
@@ -12,38 +11,11 @@ class ChatActivity(
     private val objPath = "org.telegram.ui.ChatActivity"
 
     private val fieldDialogId by lazy { resolver.getField(objPath, "dialog_id") }
-    private val methodScrollToMessageId by lazy { resolver.getMethod(objPath, "scrollToMessageId") }
-    private val methodUpdatePagedownButtonVisibility by lazy { resolver.getMethod(objPath, "updatePagedownButtonVisibility") }
     private val fieldCanShowPagedownButton by lazy { resolver.getField(objPath, "canShowPagedownButton") }
     private val fieldPagedownButtonShowedByScroll by lazy { resolver.getField(objPath, "pagedownButtonShowedByScroll") }
 
     val dialogId: Long
         get() = getLongField(instance, fieldDialogId)
-
-    fun scrollToMessageId(
-        id: Int,
-        fromMessageId: Int,
-        select: Boolean,
-        loadIndex: Int,
-        forceScroll: Boolean,
-        forcePinnedMessageId: Int,
-    ) = callMethod(
-        instance,
-        methodScrollToMessageId,
-        id,
-        fromMessageId,
-        select,
-        loadIndex,
-        forceScroll,
-        forcePinnedMessageId,
-    )
-
-    fun updatePagedownButtonVisibility(animated: Boolean) =
-        callMethod(
-            instance,
-            methodUpdatePagedownButtonVisibility,
-            animated,
-        )
 
     var canShowPagedownButton: Boolean
         get() = getBooleanField(instance, fieldCanShowPagedownButton)
